38a5b104feeeedf72cccdd19eceb16c4d5068d69
githubaa2020 <antavidel@gmail.com>
2020-11-09 23:07:04 +0100

    first commit

--- src/regex001.js
+++ src/regex001.js
@@ -1,24 +1,26 @@
 /* Insertar datos en la colección COMPONENTES creados por el propio alumno */
 
+
 db.componentes.insertMany([
     { "identificador" : 1, "pieza_numerada" : "proc1999_1","detalle_componente" : "PENTIUM obsoleto" },
-    { "identificador" : 2, "pieza_numerada" : "proc1999_2", "detalle_componente" : "PENTIUM obsoleto\estropeado" },
+    { "identificador" : 2, "pieza_numerada" : "proc1999_2", "detalle_componente" : "PENTIUM obsoleto\nestropeado" },
     { "identificador" : 3, "pieza_numerada" : "mem2016_1", "detalle_componente" : "     RAM DDR2" },
-    { "identificador" : 4, "pieza_numerada" : "nic2016_1", "detalle_componente" : "Tarjeta de red\con wifi" },
+    { "identificador" : 4, "pieza_numerada" : "nic2016_1", "detalle_componente" : "Tarjeta de red\ncon wifi" },
     { "identificador" : 5, "pieza_numerada" : "proc1999_3","detalle_componente" : "pentium obsoleto" },
-    { "identificador" : 6, "pieza_numerada" : "proc1999_4", "detalle_componente" : "PENTIUM obsoleto\funcional" },
+    { "identificador" : 6, "pieza_numerada" : "proc1999_4", "detalle_componente" : "PENTIUM obsoleto\nfuncional" },
     { "identificador" : 7, "pieza_numerada" : "mem2018_1", "detalle_componente" : "     ram DDR3" },
-    { "identificador" : 21, "pieza_numerada" : "nic2019_1", "detalle_componente" : "Tarjeta de red\con wifi" },
+    { "identificador" : 21, "pieza_numerada" : "nic2019_1", "detalle_componente" : "Tarjeta de red\ncon wifi" },
     { "identificador" : 22, "pieza_numerada" : "proc2004_1","detalle_componente" : "pentium\funcional" },
-    { "identificador" : 10, "pieza_numerada" : "proc1999_6", "detalle_componente" : "PENTIUM obsoleto\funcional" },
+    { "identificador" : 10, "pieza_numerada" : "proc1999_6", "detalle_componente" : "PENTIUM obsoleto\nfuncional" },
     { "identificador" : 11, "pieza_numerada" : "mem2020_1", "detalle_componente" : "RAM DDR4" },
-    { "identificador" : 12, "pieza_numerada" : "nic2019_2", "detalle_componente" : "Tarjeta de red\sin wifi" },
+    { "identificador" : 12, "pieza_numerada" : "nic2019_2", "detalle_componente" : "Tarjeta de red\nsin wifi" },
     { "identificador" : 13, "pieza_numerada" : "proc2020_1","detalle_componente" : "XEON octa-core" },
-    { "identificador" : 14, "pieza_numerada" : "proc2020_2", "detalle_componente" : "xeon dual-core\estropeado" },
+    { "identificador" : 14, "pieza_numerada" : "proc2020_2", "detalle_componente" : "xeon dual-core\nestropeado" },
     { "identificador" : 15, "pieza_numerada" : "mem2020_2", "detalle_componente" : "     RAM DDR4" },
-    { "identificador" : 16, "pieza_numerada" : "nic2020_1", "detalle_componente" : "Tarjeta de red\con WIFI" },
+    { "identificador" : 16, "pieza_numerada" : "nic2020_1", "detalle_componente" : "Tarjeta de red\ncon WIFI" },
     { "identificador" : 17, "pieza_numerada" : "proc2020_3","detalle_componente" : "XEON OCTA-core" },
-    { "identificador" : 23, "pieza_numerada" : "proc2020_4", "detalle_componente" : "xeon dual-CORE\estropeado" },
+    { "identificador" : 23, "pieza_numerada" : "proc2020_4", "detalle_componente" : "xeon dual-CORE\nestropeado" },
     { "identificador" : 24, "pieza_numerada" : "mem2020_3", "detalle_componente" : "     RAM ddr4" },
-    { "identificador" : 20, "pieza_numerada" : "nic2020_2", "detalle_componente" : "Tarjeta de red\SIN WIFI" },    
+    { "identificador" : 20, "pieza_numerada" : "nic2020_2", "detalle_componente" : "Tarjeta de red\nSIN WIFI" },
+    
     ]);
